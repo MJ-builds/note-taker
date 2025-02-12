74148022f48cc40a78ff9d2a097e6594ea3106e5
MJ-builds <mj@M.>
2025-02-12 12:22:00 +0200

    test

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -39,16 +39,8 @@ export default function Login() {
               Email address
             </label>
             <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
               <Input
-                className="bg-orange-500"
+                className=""
                 id="email2"
                 name="email2"
                 type="email"
